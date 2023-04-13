e427249919de0b4e2ed01839a2e8322f4a5400aa
jasking10 <jasking@chapman.edu>
2023-04-13 07:33:12 -0800

    Update StonkerGame.py

--- src/StonkerGame.py
+++ src/StonkerGame.py
@@ -6,11 +6,12 @@ gameClock=pygame.time.Clock()
 MainDisplay=pygame.display.set_mode((1250,675))
 MainDisplay.fill('Gray')
 AssetSelectionMenu=assetSelectionMenu.assetSelectionMenu(MainDisplay)
+AssetSelectionMenuIsActive=True
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
-    AssetSelectionMenu.draw(False)
+    AssetSelectionMenu.draw(AssetSelectionMenuIsActive)
     pygame.display.update()
 
